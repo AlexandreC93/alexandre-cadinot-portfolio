a223bad884a8875d16e749e158ce8b2487c6adec
AlexandreC93 <alexandrecadinot93@gmail.com>
2025-11-13 23:40:20 +0100

    import insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
       <body className="min-h-screen bg-slate-50 text-slate-900 antialiased dark:bg-slate-950 dark:text-slate-50">
         <Nav />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
